55151ddb9c78ac8e9c4422fc7f7f5a5e131fcd70
Lalo Herrera <63426075+EduardoHerr@users.noreply.github.com>
2021-08-02 14:48:09 -0500

    Logeo

--- ProyectoMovil/MainActivity.cs
+++ ProyectoMovil/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
 
 namespace ProyectoMovil
@@ -8,12 +9,45 @@ namespace ProyectoMovil
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        connectDB db = new connectDB();
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            var user = FindViewById<EditText>(Resource.Id.txtUser);
+            var pwd = FindViewById<EditText>(Resource.Id.txtClave);
+            var btnIngresar = FindViewById<Button>(Resource.Id.button1);
+
+            btnIngresar.Click += delegate
+            {
+				if (db.Ingresar(user.Text,pwd.Text)==0)
+				{
+                    Toast.MakeText(this, "Usuario Incorrecto", ToastLength.Short).Show();
+                    limpiarCampos();
+				}
+				else if (db.Ingresar(user.Text, pwd.Text) == 1)
+				{
+                    Toast.MakeText(this, "Bienvenido Administrador", ToastLength.Short).Show();
+                    limpiarCampos();
+                }
+                else
+				{
+                    Toast.MakeText(this, "Bienvenido Usuario", ToastLength.Short).Show();
+                    limpiarCampos();
+                }
+
+            };
+
+
+            void limpiarCampos()
+			{
+                user.Text = pwd.Text = "";
+			}
+
+
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
